e0b5e4cfd25a8246f3d7bf237fdb5532ab608203
lleal <742@holbertonschool.com>
2020-01-10 03:52:28 +0000

    private class attr

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,7 +2,7 @@
 class Square():
     __size = 0
 
-    def __init__(self, new_size):
+    def __init__(self, new_size=0):
         """Initialize class."""
-        if new_size is not None:
+        if new_size is not 0:
             self.__size = new_size
