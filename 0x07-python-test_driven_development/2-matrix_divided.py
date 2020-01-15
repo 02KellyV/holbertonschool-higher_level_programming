cab0e6faf199487fabb637d321c3505d6751b95d
02KellyV <900@holbertonschool.com>
2020-01-15 03:29:55 +0000

    0x08

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """ div list of lists elements"""
+
+    for i 
+    if (not isinstance(div, int) and not isinstance(div, float)):
+        raise TypeError("div must be a number")
+
+    if (not isinstance(div, int) and not isinstance(div, float)):
+        raise TypeError("div must be a number") 
