bb6ae1464c8947fc812fbe440cf3b32d3a427a20
02KellyV <900@holbertonschool.com>
2020-04-07 16:24:41 +0000

    City relationship

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-""" Class definition of State. Instance Base = declarative_base() """
+#!/usr/bin/python2
+""" Class definition of State InstanceBase = declarative_base() """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
