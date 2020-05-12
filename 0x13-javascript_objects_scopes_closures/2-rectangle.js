39c781c221d7cd2a46bd72ff3938291aa59473f8
paulinaALV <velezpaulina139@gmail.com>
2020-05-11 21:13:30 -0500

    Fix logic, task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    //If w or h is equal to 0 or not a positive integer, create empty object
+    if (w > 1 && h > 1 )
+    {
+      this.width = w;
+      this.height = h;
+    }
   }
 };
