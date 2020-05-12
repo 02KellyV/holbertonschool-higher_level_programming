2632a857f0e7aff570fc4fecf0c0a913bcb522f3
02KellyV <900@holbertonschool.com>
2020-05-12 13:53:39 -0500

    7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
-    for (let i in list) {
-      if (list[i] === searchElement) {
-        count++;
-      }
+  let count = 0;
+  for (const i of list) {
+    if (i === searchElement) {
+      count++;
     }
-    return count;
-  };
+  }
+  return count;
+};
