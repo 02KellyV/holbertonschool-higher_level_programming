accc9bc236b639dcd34faff09a1fadfc5f19ec19
02KellyV <900@holbertonschool.com>
2020-05-12 13:44:18 -0500

    Fix logic base clas

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     // If w or h is equal to 0 or not a positive integer, create empty object
-    if (w > 1 && h > 1) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
