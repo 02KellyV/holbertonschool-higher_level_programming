8857b315a234565661086fe08797058ab8a53cca
02KellyV <900@holbertonschool.com>
2020-05-15 23:26:15 +0000

    Correct semistandard - Endpoint

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const url = "https://swapi-api.hbtn.io/api/films/"
-request (url + process.argv[2], (err, response, body) => {
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
